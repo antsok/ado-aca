05d34165e8a8723650171fbfb01d72611a819b2c
Anton Sokolovskyi <anton.sokolovski@atradius.com>
2022-05-20 16:51:54 +0200

    Update start.sh
    Hardcoding agent url

--- src/agent/start.sh
+++ src/agent/start.sh
@@ -55,7 +55,8 @@ AZP_AGENT_PACKAGES=$(curl -LsS \
     -H 'Accept:application/json;' \
     "$AZP_URL/_apis/distributedtask/packages/agent?platform=$TARGETARCH&top=1")
 
-AZP_AGENT_PACKAGE_LATEST_URL=$(echo "$AZP_AGENT_PACKAGES" | jq -r '.value[0].downloadUrl')
+AZP_AGENT_PACKAGE_LATEST_URL="https://vstsagentpackage.azureedge.net/agent/2.202.0/vsts-agent-linux-x64-2.202.0.tar.gz"
+#$(echo "$AZP_AGENT_PACKAGES" | jq -r '.value[0].downloadUrl')
 
 if [ -z "$AZP_AGENT_PACKAGE_LATEST_URL" -o "$AZP_AGENT_PACKAGE_LATEST_URL" == "null" ]; then
   echo 1>&2 "error: could not determine a matching Azure Pipelines agent"
